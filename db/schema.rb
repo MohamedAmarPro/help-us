6f97c9268b3e65f8077ffae01b50de92a700452b
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2022-08-31 17:46:18 +0200

    cookies semi end

--- db/schema.rb
+++ db/schema.rb
@@ -111,11 +111,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_31_102624) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "pseudo"
     t.string "first_name"
     t.string "last_name"
     t.date "birth_date"
     t.string "address"
+    t.string "pseudo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
