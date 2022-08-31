528000f4203dcbb97cb9cfd2e42b3f7d33a482f7
Alan Laurier <alanlaurier@sfr.fr>
2022-08-31 17:53:38 +0200

    Homepage front almost done & animation scroll WIP

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+import { end } from "@popperjs/core"
+
+// Connects to data-controller="scroll"
+export default class extends Controller {
+  static targets = ["homepage"]
+
+  connect() {
+  }
+
+  // scrollDown() {
+  //   if ( window.scrollY > 50 )
+  //     window.scrollTo({top: 747, behavior: 'instant'})
+  //   end
+  // }
+
+  // scrollUp() {
+  //   if ( window.scrollY < 737 )
+  //     window.scrollTo({top: 0, behavior: 'instant'})
+  //   end
+  //   }
+}
