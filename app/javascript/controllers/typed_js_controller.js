5a87638848ff9c3452606b8381fa15a662fc9433
Titouan Andritsos <titouan.andritsos@hotmail.fr>
2022-09-06 11:22:11 +0200

    navbar scroll hiding and no longer on hp

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,7 +5,7 @@ import Typed from "typed.js"
 export default class extends Controller {
   connect() {
     new Typed(this.element, {
-      strings: ['La force du don humanitaire ne demeure pas que dans le montant, mais bien dans le bon emploi du montant.', "Dans l'humanitaire, le coeur ne suffit pas ; il nous faut aussi des fonds.", 'Si j’aide une seule personne à avoir de l’espoir, je n’aurais pas vécu en vain.', "À travers l‘autre, nos enfants, nos parents, nos amis, c‘est aussi l‘Humanité que nous aidons."],
+      strings: ["'It is in your hands, to make a better world for all who live in it.'                          Nelson Mandela"],
       typeSpeed: 50,
       loop: true,
       fadeOut: true,
