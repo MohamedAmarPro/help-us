1be79345cf1f1d804c60bfa7138821b1fc8d299e
AitAmar Mohamed <muhamed.aitamar@gmail.com>
2022-09-07 11:57:46 +0200

    end

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.hosts << 'https://helpus-b36.herokuapp.com/'
+  
 
   # Use a different logger for distributed setups.
   # require "syslog/logger"
